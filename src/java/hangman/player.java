48dff924c40f0a2954b0c2feada8ad18afec635f
Tyler Drury <c0661137@mylambton.ca>
2017-04-05 12:14:36 -0400

    fixed default package

--- src/java/hangman/player.java
+++ src/java/hangman/player.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package hangman;
 
 /**
  *
- * @author c0660455
+ * @author c0661137
  */
-public class Placeholder {
+public class player {
     
 }
