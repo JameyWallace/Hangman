6436e57af0123f1d2ca64549425ed1754bac5160
Jamey Wallace <c0660455@mylambton.ca>
2017-04-05 13:43:23 -0400

    display class almost finished

--- src/java/Class/TheMan.java
+++ src/java/Class/TheMan.java
@@ -5,6 +5,8 @@
  */
 package Class;
 
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import javax.swing.JLabel;
 
 /**
@@ -38,13 +40,74 @@ public class TheMan extends JLabel
     
     
     /**
-     * 
+     * The folder that images are in
      */
     private String Image_Directory;
     
     
-    
+    /**
+     * jpg, png, mpeg 
+     */
     private String Image_Type;
+    
+    
+    /**
+     * the path of the current image
+     */
+    private String path;
+    
+    
+    /**
+     * image that is being displayed
+     */
+    private BufferedImage Buffer;
+    
+    
+    
+    private BufferedImage loadImage(String path) 
+    {   
+        return null;
+    }
+    
+   private Dimension newDimension(int WIDTH, int HEIGHT) 
+   {
+       return null;
+   }
+
+    
+    
+    /**
+     * default constructor
+     */
+    public TheMan(){
+        
+        this ("man", "design/", ".png");
+        
+    }
+    /**
+     * Creates the hangman using given information
+     * @param img the name of the image
+     * @param imgPlace where it is stored
+     * @param imgType What kind of image it is
+     */
+    public TheMan(String img, String imgPlace, String imgType)
+    {
+        WIDTH = 440;
+        HEIGHT = 260;
+        
+        Image_Name = img;
+        Image_Directory = imgPlace;
+        Image_Type = imgType;
+        
+        /**
+         *  the "_0" needs to be there so the the man_1-5 can be selected
+         */
+        setPreferredSize(newDimension(WIDTH,HEIGHT));
+        path = Image_Directory + Image_Name + "_0" +  Image_Type;
+        Buffer = loadImage(path);
+        
+    }
+    
     
     
 }
