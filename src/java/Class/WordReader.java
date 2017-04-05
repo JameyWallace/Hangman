1b3df1318bce4fc31cb88c72d5408f5a52f0399c
Michael Snopko <michael.snopko@gmail.com>
2017-04-05 12:41:41 -0400

    plz work

--- src/java/Class/WordReader.java
+++ src/java/Class/WordReader.java
@@ -5,10 +5,37 @@
  */
 package Class;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
  *
  * @author c0638820
  */
 public class WordReader {
+    private static final String fileName = "";
     
+    private ArrayList<String> words = new ArrayList<String>();
+    public WordReader(){
+    try (InputStream in = getClass().getResourceAsStream(fileName);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(in))) {
+        
+        String line = "";
+        while ((line = bf.readLine()) != null)
+            //word.add(line);
+            return ;
+        }
+    
+        catch (Exception e){
+                System.out.println("Didn't find/read file:" + fileName);
+                System.out.println("Error Message:" + e.getMessage());
+            }
+        }
+    //Call to Database
+//    public String getRandomWord(){
+//        if(word.isEmpty()) return "No Data";
+//        return word.get((int)(Math.random()word.size()));
+//    }
 }
