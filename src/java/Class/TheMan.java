6e6e7a7198c2e691a19a9e23f27ea8450429be15
Jamey Wallace <c0660455@mylambton.ca>
2017-04-05 12:41:22 -0400

    Creation of the hangman display class

---
+++ src/java/Class/TheMan.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author c0660455
+ */
+
+
+/**
+ *  Using the Jlabel Library to display the images used for hangman
+ * @author c0660455
+ */
+public class TheMan extends JLabel
+{
+    /**
+     * The images width
+     */
+    private int WIDTH;
+   
+    
+    /**
+     * The images Height
+     */
+    private int HEIGHT;
+   
+    
+    /**
+     * The name of the image that will be used (man_0-5)
+     */
+    private String Image_Name;
+    
+    
+    /**
+     * 
+     */
+    private String Image_Directory;
+    
+    
+    
+    private String Image_Type;
+    
+    
+}
